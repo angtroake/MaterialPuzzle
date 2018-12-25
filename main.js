a6b1bf29550201eefe0a329ad915a1b6f98b6f35
angtroake <angtroake@mun.ca>
2018-12-25 02:28:30 -0330

    Window Ajusting Handling and stars

--- main.js
+++ main.js
@@ -38,6 +38,14 @@ childWindow.onload = function(){
 //20 ticks per second
 setInterval(function(){tick();render();}, 1000/ticksPerSecond);
 
+
+
+//ADD STARS
+/*canvas.onclick = function(e){
+    data.stars.push([e.offsetX/canvas.width, e.offsetY/canvas.height, 1]);
+    console.log(data.stars);
+}*/
+
 /**
  *  Events
  */
@@ -54,7 +62,13 @@ function render(){
     ctx.fillRect(0,0,canvas.width,canvas.height);
 
     //render
-
+    let starAlpha = (time > maxTime/4 && time < maxTime*3/4)? Math.abs(Math.abs(0.5 -time/maxTime)-0.25)*10 : 0;
+    data.stars.forEach(function(star){
+        ctx.beginPath();
+        ctx.arc(canvas.width*star[0], canvas.height*star[1], star[2], 0, 2*Math.PI, false);
+        ctx.fillStyle = "rgba(255,255,255," + starAlpha + ")";
+        ctx.fill();
+    });
 
     //Render Sun and moon
     ctx.beginPath();
@@ -72,6 +86,7 @@ function render(){
     ctx.fill();
 
 
+    //rendering ground
     ctx.fillStyle = 'hsl(122, 39%, ' + (32 + 10*(Math.cos(time/(maxTime/(2*Math.PI))))) + '%)';
 
     ctx.fillRect(0, canvas.height - canvas.height/4, canvas.width, canvas.height/2);
@@ -80,9 +95,39 @@ function render(){
 function tick(){
     time++;
 
+    if(window.innerWidth < 1000){
+        canvas.width = 1000;
+    }else if(window.innerWidth > 1300){
+        canvas.width = 1300;
+    }else{
+        canvas.width = window.innerWidth;
+    }
+
+    canvas.height = canvas.width * 3/5;
+
+    /*if(window.innerHeight < 750){
+        canvas.height = 750;
+    }else if(window.innerHeight > 975){
+        canvas.height = 975
+    }else{
+        canvas.height = window.innerHeight;
+    }*/
+   
+
     if(time > maxTime){
         time = 0;
     }else if(time < 0){
         time = maxTime;
     }
+}
+
+
+var data = {
+    "stars":[
+        [0.2,0.2,1],
+        [0.15923076923076923, 0.23846153846153847, 1],[0.17615384615384616, 0.2858974358974359, 1],[0.2153846153846154, 0.25384615384615383, 1],[0.21307692307692308, 0.1358974358974359, 1],[0.24615384615384617, 0.08974358974358974, 1],[0.29307692307692307, 0.08974358974358974, 1],[0.39, 0.18461538461538463, 1],[0.4176923076923077, 0.24871794871794872, 1],[0.45384615384615384, 0.2012820512820513, 1],
+        [0.4876923076923077, 0.25, 1],[0.52, 0.18974358974358974, 1],[0.28923076923076924, 0.36025641025641025, 1],[0.6292307692307693, 0.46153846153846156, 1],[0.6892307692307692, 0.12051282051282051, 1],[0.8192307692307692, 0.23076923076923078, 1],[0.61, 0.26666666666666666, 1],[0.4369230769230769, 0.4948717948717949, 1],[0.1076923076923077, 0.10384615384615385, 1],
+        [0.04153846153846154, 0.49743589743589745, 1],[0.23076923076923078, 0.5076923076923077, 1],[0.7661538461538462, 0.3641025641025641, 1],[0.7446153846153846, 0.5576923076923077, 1],
+        [0.9192307692307692, 0.4166666666666667, 1],[0.8638461538461538, 0.07948717948717948, 1],[0.5561538461538461, 0.5717948717948718, 1],[0.5715384615384616, 0.591025641025641, 1],[0.5884615384615385, 0.6064102564102564, 1],[0.6692307692307692, 0.5423076923076923, 1],[0.68, 0.47307692307692306, 1],[0.5130769230769231, 0.6038461538461538, 1],[0.5753846153846154, 0.6602564102564102, 1]
+    ]
 }
