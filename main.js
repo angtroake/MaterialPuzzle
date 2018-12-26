50b370c23f87053246c5e158a8ba059493538ca0
angtroake <angtroake@mun.ca>
2018-12-25 21:50:41 -0330

    Cloud Mechanic and Buildings

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 /**
- * Variable Setup
+ * GLOBAL Variable Setup
  */
 var canvas = document.getElementById("canvasMain");
 var ctx = canvas.getContext("2d");
 
 var ticksPerSecond = 20;
+
+var maxCanvasWidth = 1300;
+var minCanvasWidth = 1000;
+
 
 /**
  * Map Variables
@@ -22,6 +26,9 @@ var sunX = 0;
 var sunY = 0;
 var moonX = 0;
 var moonY = 0;
+
+var isRaining = false;
+var rainOpacity = 0;
 
 var mapObjects = [];
 
@@ -84,8 +91,6 @@ function render(){
 
     //Render Sun and moon
     ctx.beginPath();
-    
-
     ctx.arc(sunX, sunY, 75, 0, 2*Math.PI, false);
     ctx.fillStyle = 'rgb(255, 235, 59)';
     ctx.fill();
@@ -94,22 +99,21 @@ function render(){
     ctx.fillStyle = 'white';
     ctx.fill();
 
-
-    //rendering ground
-    ctx.fillStyle = 'hsl(122, 39%, ' + (32 + 10*(Math.cos(time/(maxTime/(2*Math.PI))))) + '%)';
-    /*ctx.beginPath();
-    ctx.lineWidth = 4;
-    ctx.moveTo(0, canvas.height);
-    ctx.bezierCurveTo(-canvas.width/2,canvas.height,-300,0,canvas.width, canvas.height);
-    ctx.fill();*/
-
-    ctx.fillRect(0, canvas.height - canvas.height/4, canvas.width, canvas.height/2);
+    if(rainOpacity > 0){
+        ctx.fillStyle='rgba(200,200,200,' + rainOpacity + ')';
+        ctx.fillRect(50,50,100,100);
+    }
 
 
     mapObjects.forEach(function(mo){
         mo.render();
     });
 
+
+
+    //rendering ground
+    ctx.fillStyle = 'hsl(122, 39%, ' + (32 + 10*(Math.cos(time/(maxTime/(2*Math.PI))))) + '%)';
+    ctx.fillRect(0, canvas.height - canvas.height/4, canvas.width, canvas.height/2);
 }
 
 function tick(){
@@ -124,16 +128,24 @@ function tick(){
 
     //Time state Handle
     if(time > maxTime/4 && time < maxTime*3/4){
+        if(isRaining && timeState == timeStateDay){
+            isRaining = false;
+        }
         timeState = timeStateNight;
     }else{
+        if(timeState == timeStateNight){
+            if(Math.floor(Math.random()*2) == 0){
+                isRaining = true;
+            }
+        }
         timeState = timeStateDay;
     }
 
     //Canvas Size Adjust
-    if(window.innerWidth < 1000){
-        canvas.width = 1000;
-    }else if(window.innerWidth > 1300){
-        canvas.width = 1300;
+    if(window.innerWidth < minCanvasWidth){
+        canvas.width = minCanvasWidth;
+    }else if(window.innerWidth > maxCanvasWidth){
+        canvas.width = maxCanvasWidth;
     }else{
         canvas.width = window.innerWidth;
     }
@@ -144,6 +156,13 @@ function tick(){
     sunY = (canvas.height - canvas.height/4) + canvas.height/1.5*Math.sin(time/(maxTime/(2*Math.PI)) - Math.PI/2);
     moonX = (canvas.width/2) + canvas.height/1.5*Math.cos(time/(maxTime/(2*Math.PI)) - Math.PI/2);
     moonY = (canvas.height - canvas.height/4) - canvas.height/1.5*Math.sin(time/(maxTime/(2*Math.PI)) - Math.PI/2);
+
+    if(isRaining && rainOpacity < 1){
+        rainOpacity+=0.05;
+    }
+    else if(!isRaining && rainOpacity > 0)
+        rainOpacity-=0.05;
+
 
     mapObjects.forEach(function(mo){
         mo.tick();
@@ -193,19 +212,35 @@ class Mountain{
 
     }
 }
+
 /**
  * 
  *    BUILDING
  * 
  * 
  */
-class Building{
-    constructor(){
 
+class Building{
+    constructor(x, y, type, h, s, l){
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.h = h;
+        this.s = s;
+        this.l = l;
     }
 
     render(){
 
+        let mapX = this.x * canvas.width;
+        let mapY = this.y * canvas.height;
+
+        ctx.fillStyle = 'hsl(' + this.h + ',' + this.s +  '%,' + (this.l + 10*(Math.cos(time/(maxTime/(2*Math.PI))))) + '%)';
+        if(this.type == 0){
+            ctx.fillRect(mapX, mapY, 50, -50);
+        }else if(this.type == 1){
+            ctx.fillRect(mapX, mapY, 40, -40);
+        }
     }
 
     tick(){
@@ -216,29 +251,31 @@ class Building{
 
 var data = {
     "stars":[
-        [0.2,0.2,1],
-        [0.15923076923076923, 0.23846153846153847, 1],[0.17615384615384616, 0.2858974358974359, 1],[0.2153846153846154, 0.25384615384615383, 1],[0.21307692307692308, 0.1358974358974359, 1],[0.24615384615384617, 0.08974358974358974, 1],[0.29307692307692307, 0.08974358974358974, 1],[0.39, 0.18461538461538463, 1],[0.4176923076923077, 0.24871794871794872, 1],[0.45384615384615384, 0.2012820512820513, 1],
+        [0.2,0.2,1],[0.15923076923076923, 0.23846153846153847, 1],[0.17615384615384616, 0.2858974358974359, 1],[0.2153846153846154, 0.25384615384615383, 1],[0.21307692307692308, 0.1358974358974359, 1],[0.24615384615384617, 0.08974358974358974, 1],[0.29307692307692307, 0.08974358974358974, 1],[0.39, 0.18461538461538463, 1],[0.4176923076923077, 0.24871794871794872, 1],[0.45384615384615384, 0.2012820512820513, 1],
         [0.4876923076923077, 0.25, 1],[0.52, 0.18974358974358974, 1],[0.28923076923076924, 0.36025641025641025, 1],[0.6292307692307693, 0.46153846153846156, 1],[0.6892307692307692, 0.12051282051282051, 1],[0.8192307692307692, 0.23076923076923078, 1],[0.61, 0.26666666666666666, 1],[0.4369230769230769, 0.4948717948717949, 1],[0.1076923076923077, 0.10384615384615385, 1],
         [0.04153846153846154, 0.49743589743589745, 1],[0.23076923076923078, 0.5076923076923077, 1],[0.7661538461538462, 0.3641025641025641, 1],[0.7446153846153846, 0.5576923076923077, 1],
         [0.9192307692307692, 0.4166666666666667, 1],[0.8638461538461538, 0.07948717948717948, 1],[0.5561538461538461, 0.5717948717948718, 1],[0.5715384615384616, 0.591025641025641, 1],[0.5884615384615385, 0.6064102564102564, 1],[0.6692307692307692, 0.5423076923076923, 1],[0.68, 0.47307692307692306, 1],[0.5130769230769231, 0.6038461538461538, 1],[0.5753846153846154, 0.6602564102564102, 1]
+    ],
+    "mountains":[
+        [0.07,0.75,300,300], [0.2,0.75,200,200]
+    ],
+    "buildings":[
+        [0.8,0.75,0, 0, 70, 40],[0.82, 0.75, 1, 240, 67, 58]
     ]
 }
 
 
+data.mountains.forEach(function(m){
+    mapObjects.push(new Mountain(m[0], m[1], m[2], m[3]));
+});
+
+data.buildings.forEach(function(b){
+    let build = new Building(b[0], b[1], b[2], b[3], b[4], b[5]);
+    mapObjects.push(build);
+});
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-mapObjects.push(new Mountain(0.1, 0.75, 200, 200));
+mapObjects.push(new Mountain(0.07, 0.75, 300, 300));
+mapObjects.push(new Mountain(0.2, 0.75, 200, 200));
